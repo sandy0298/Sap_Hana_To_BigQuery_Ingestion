262393f78a088cbedd8055793116d42ec90c732e
Sandeep Mohanty <86830610+sandy0298@users.noreply.github.com>
2023-02-10 22:04:25 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,1 @@
-python3 /home/project_id/sapdataflow/dataflow_sap1.py --runner DataflowRunner --project sandy-376313 --region us-central1 --temp_location  gs://sap_poc/temp    --job_name sapjobload5  --setup_file /home/project_id/setup.py --dfBucket "sap_poc"     
+python3 /home/project_id/sapdataflow/dataflow_sap1.py --runner DataflowRunner --project project_id --region us-central1 --temp_location  gs://sap_poc/temp    --job_name sapjobload5  --setup_file /home/project_id/setup.py --dfBucket "sap_poc"     
